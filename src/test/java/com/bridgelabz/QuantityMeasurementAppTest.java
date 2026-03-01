ab0ccf234b02f3f96bf7ccdf95a39799420a9b9c
Shivak007 <kshivaoffl2824@gmail.com>
2026-03-02 01:25:56 +0530

    [Shiva Kumar] : UC1 - Feet measurement equality

---
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 =
+                new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 =
+                new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 =
+                new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+}
